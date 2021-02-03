5b7207b0f26c9f8d50dc6f3a1d035505bda8d304
brockbyrd <brockbyrdd@gmail.com>
2021-02-03 10:40:42 -0600

    makes use of Provider and create store in index.js

--- superhero-frontend/src/App.js
+++ superhero-frontend/src/App.js
@@ -34,7 +34,14 @@ class App extends Component {
 
   render() {
     return(
-      <Homepage />
+      <div className="parent">
+
+        <div className="leftHalf" onClick={this.filterGood} style={{ backgroundImage: `url("https://www.comicbasics.com/wp-content/uploads/2020/08/The-Top-10-Greatest-Superheroes-Without-Superpowers-In-Comics-Today.jpg")`}}></div>
+
+        <div className="rightHalf" onClick={this.filterEvil} style={{ backgroundImage: `url("https://static1.cbrimages.com/wordpress/wp-content/uploads/2019/11/Meanest-Supervillains-Of-All-Time.jpg")`}}></div>
+
+      </div>
+
     )
   }
 }
