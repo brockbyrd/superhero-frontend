8e8d167155803b11e8757a5159ffdffac04e4e98
brockbyrd <brockbyrdd@gmail.com>
2021-02-03 13:06:34 -0600

    gets rid of local state in app.js

--- superhero-frontend/src/App.js
+++ superhero-frontend/src/App.js
@@ -4,19 +4,20 @@ import { fetchCharacters } from './actions/characterActions'
 import { connect } from 'react-redux'
 
 class App extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      characters: []
-    }
-  }
+  // constructor(props) {
+  //   super(props);
+  //   this.state = {
+  //     characters: []
+  //   }
+  // }
 
   componentDidMount() {
     this.props.fetchCharacters();
+    console.log(this.props.characters)
   }
 
     filterGood = () => {
-      let heroes = this.state.characters
+      let heroes = this.props.characters
       heroes = heroes.filter((char) =>
         char.alignment === 'good'
       )
@@ -24,7 +25,7 @@ class App extends Component {
     }
 
     filterEvil = () => {
-      let villains = this.state.characters
+      let villains = this.props.characters
       villains = villains.filter((char) =>
       char.alignment === 'bad'
       )
@@ -35,9 +36,9 @@ class App extends Component {
     return(
       <div className="parent">
 
-        <div className="leftHalf" onClick={this.filterGood} style={{ backgroundImage: `url("https://www.comicbasics.com/wp-content/uploads/2020/08/The-Top-10-Greatest-Superheroes-Without-Superpowers-In-Comics-Today.jpg")`}}></div>
+        <div className="leftHalf" onClick={this.filterGood}><img src="https://www.wallpapertip.com/wmimgs/35-357728_all-superheroes-wallpaper.jpg" alt="" /></div>
 
-        <div className="rightHalf" onClick={this.filterEvil} style={{ backgroundImage: `url("https://static1.cbrimages.com/wordpress/wp-content/uploads/2019/11/Meanest-Supervillains-Of-All-Time.jpg")`}}></div>
+        <div className="rightHalf" onClick={this.filterEvil}><img src="https://wallpaperaccess.com/full/783564.jpg" alt="" /></div>
 
       </div>
 
