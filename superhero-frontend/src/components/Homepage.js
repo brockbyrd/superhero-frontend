c7812af7da1bbbf769ebfb942dfbc6c312e93539
brockbyrd <brockbyrdd@gmail.com>
2021-02-04 08:07:22 -0600

    creates cards for superheros

--- superhero-frontend/src/components/Homepage.js
+++ superhero-frontend/src/components/Homepage.js
@@ -8,11 +8,11 @@ class Homepage extends Component {
 		return(
             <div className="parent">
                 <Route render={({ history }) => (
-                <div className="leftHalf" onClick={() => history.push('/superheroes')}><img src="https://www.wallpapertip.com/wmimgs/35-357728_all-superheroes-wallpaper.jpg" alt="" onClick={this.props.filterGood} /></div>
+                <div className="leftHalf" onClick={() => history.push('/superheroes')}><img className="homepage" src="https://www.wallpapertip.com/wmimgs/35-357728_all-superheroes-wallpaper.jpg" alt="" onClick={this.props.filterGood} /></div>
                 )
             } />
                 <Route render={({ history }) => (
-                <div className="rightHalf" onClick={() => history.push('/villains')} ><img src="https://wallpaperaccess.com/full/783564.jpg" alt="" onClick={this.props.filterEvil} /></div>
+                <div className="rightHalf" onClick={() => history.push('/villains')} ><img className="homepage" src="https://wallpaperaccess.com/full/783564.jpg" alt="" onClick={this.props.filterEvil} /></div>
                 )} />
           </div>
 		)
--- superhero-frontend/src/components/Superhero.js
+++ superhero-frontend/src/components/Superhero.js
@@ -1,11 +1,30 @@
 import React, { Component } from 'react';
+import Card from 'react-bootstrap/Card';
 
 class Superhero extends Component {
 
 	render(){
 		return(
-            <div>
-                {this.props.heroes.map((hero) => hero.name)}
+            <div className="flex-container">
+				{this.props.heroes.map((hero, index) =>
+				<div className="cards">
+					<Card key={index} style={{ width: '18rem' }}>
+						<Card.Img variant="top" src={hero.image1} />
+						<Card.Body>
+						<Card.Title>{hero.name}</Card.Title>
+						<Card.Text>
+							<ul>
+								<li>Intelligence: {hero.intelligence}</li>
+								<li>Strength: {hero.strength}</li>
+								<li>Speed: {hero.speed}</li>
+						 		<li>Durability: {hero.durability}</li>
+						 		<li>Combat: {hero.combat}</li>
+							</ul>
+						</Card.Text>
+					</Card.Body>
+				  </Card>
+				</div>
+				)}
             </div>
 		)
 	}
