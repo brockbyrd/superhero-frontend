36df2a0d694030f55c6e1918d10413b1d79ddfce
brockbyrd <brockbyrdd@gmail.com>
2021-02-06 18:14:38 -0600

    styles homepage

--- superhero-frontend/src/components/Homepage.js
+++ superhero-frontend/src/components/Homepage.js
@@ -7,14 +7,25 @@ class Homepage extends Component {
 	render(){
 		return(
             <div className="parent">
-                <Route render={({ history }) => (
-                <div className="leftHalf" onClick={() => history.push('/superheroes')}><img className="homepage" src="https://www.wallpapertip.com/wmimgs/35-357728_all-superheroes-wallpaper.jpg" alt="" onClick={this.props.filterGood} /></div>
-                )
-            } />
 
                 <Route render={({ history }) => (
-                <div className="rightHalf" onClick={() => history.push('/villains')} ><img className="homepage" src="https://wallpaperaccess.com/full/783564.jpg" alt="" onClick={this.props.filterEvil} /></div>
-                )} />
+                    <div className="leftHalf" onClick={() => history.push('/superheroes')}>
+                        <img className="homepage" src="https://www.wallpapertip.com/wmimgs/35-357728_all-superheroes-wallpaper.jpg" alt="" onClick={this.props.filterGood} />
+                        <h2 className="superhero-text">Superheroes</h2>
+                    </div>
+                    )}
+                />
+
+
+
+                <Route render={({ history }) => (
+                    <div className="rightHalf" onClick={() => history.push('/villains')} >
+                        <img className="homepage" src="https://wallpaperaccess.com/full/783564.jpg" alt="" onClick={this.props.filterEvil} />
+                        <h2 className="villain-text">Villains</h2>
+                    </div>
+                    )}
+                />
+
             </div>
 		)
 	}
